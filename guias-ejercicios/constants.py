7bd0d64213c4122644449f5adba4aead2bda5436
akukulanski <akukulanski@gmail.com>
2019-10-07 20:28:47 -0300

    9.1 + latex + updated constants

--- guias-ejercicios/constants.py
+++ guias-ejercicios/constants.py
@@ -1,6 +1,10 @@
-#from math import pi
+from math import pi
 
 c = 3e8 # [m/s]
 e = 1.6e-19 # [C]
 hc = 1240e-9 # [eV*m]
 lo_compton = 2.43e-12 # [m]
+me = 511e3 # [eV/c^2]
+h_evs = hc/c
+h_bar = h_evs/(2*pi)
+h_Js = h_evs * e
---
+++ guias-ejercicios/equations.py
@@ -0,0 +1,7 @@
+from IPython.display import display, Math
+
+print_eq = lambda eq: display(Math(eq))
+
+incertidumbre_xp = r'\Delta x.\Delta p \ge \frac{\hbar}{2}'
+incertidumbre_Et = r'\Delta E.\Delta t \ge \frac{\hbar}{2}'
+
