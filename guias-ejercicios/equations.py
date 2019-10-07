d75cf8d5c6eccebe2dadfe326abb46a4df0146ac
akukulanski <akukulanski@gmail.com>
2019-10-07 20:41:30 -0300

    disable latex eq for github

--- guias-ejercicios/equations.py
+++ guias-ejercicios/equations.py
@@ -1,6 +1,6 @@
-from IPython.display import display, Math
-
-print_eq = lambda eq: display(Math(eq))
+#from IPython.display import display, Math
+#print_eq = lambda eq: display(Math(eq))
+print_eq = lambda dummy: 0
 
 incertidumbre_xp = r'\Delta x.\Delta p \ge \frac{\hbar}{2}'
 incertidumbre_Et = r'\Delta E.\Delta t \ge \frac{\hbar}{2}'
